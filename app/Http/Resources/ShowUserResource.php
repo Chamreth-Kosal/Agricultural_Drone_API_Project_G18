a08ac40d60434a0dced6e8707fda9db285754c4c
CHAMRETH.KOSAL <CHAMRETH.KOSAL@PNC.LAN>
2023-05-22 15:26:39 +0700

    create user or farmer

---
+++ app/Http/Resources/ShowUserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'email'=>$this->email,
+            'password'=>$this->password,
+            'drone_id'=>$this->drone_id,
+            // 'drones' =>DroneResource::collection($this->drones)
+
+        ];
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            
+            $table->unsignedBigInteger('drone_id');
+            $table->foreign('drone_id')->references('id')->on('drones')->onDelete('cascade');
             $table->timestamps();
         });
     }
